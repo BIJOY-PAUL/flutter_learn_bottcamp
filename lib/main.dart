e55eecae88119edfa90e95fa80e59640ef93135c
BIJOY-PAUL <bijoyrudrapaul78@gmail.com>
2026-06-05 19:52:32 +0600

    learn list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning/pages/homescreen.dart';
+import 'package:learning/pages/list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: Homescreen(),
+      home: listViewLearn(),
     );
   }
 }
